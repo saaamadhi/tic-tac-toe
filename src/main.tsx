69559d03b6fbab617ad4e69dd588d3553f755547
Anhelina Dashchynskaya <aaangelinavalereva@gmail.com>
2024-08-23 17:40:22 +0200

    feat: add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
 import App from './App.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '*',
+    element: <Navigate to='/' replace={true} />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
